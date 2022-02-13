ba05844b453950b90cb817ee77cd66adb3dced06
sergiuchilat <chilatsergiu@gmail.com>
2022-02-14 01:46:31 +0200

    Data binding

--- src/views/Contacts.js
+++ src/views/Contacts.js
@@ -1,13 +1,34 @@
 import React from "react";
 
-export default class extends React.Component{
-    render(){
-        return(
+export default class extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "Ion",
+            phone: "112"
+        }
+
+    }
+
+    handleNameTextChange = (e) => {
+        this.setState({ name: e.target.value });
+    }
+
+    handlePhoneTextChange = (e) => {
+        this.setState({ phone: e.target.value });
+    }
+
+    render() {
+
+        return (
             <div>
                 CONTACTS page
                 <div>
-                    Name <input type="text"/> <br/>
-                    Phone <input type="text"/> <br/>
+                    Name <input type="text" value={this.state.name} onChange={this.handleNameTextChange} /> <br />
+                    Phone <input type="text" value={this.state.phone} onChange={this.handlePhoneTextChange} /> <br />
+                </div>
+                <div>
+                    {this.state.name}, {this.state.phone}
                 </div>
             </div>
         )
